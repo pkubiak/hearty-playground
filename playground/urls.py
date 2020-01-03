97ec598fdae3f5dbff8ae71f2858321fdc00d63f
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-01-03 13:12:29 +0100

    Extracted user button to partial view

--- playground/urls.py
+++ playground/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pomoc/', include('help_app.urls')),
+    path('', RedirectView.as_view(url='pomoc/')),
 ]
